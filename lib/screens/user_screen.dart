e8a3beab54a0906b2eb9f15d72d911c1bbdfe343
Juan Pablo Garcia <juanpablo.garcia@alumni.ubc.ca>
2020-08-15 15:44:32 -0500

    Added title_widget.dart to user_screen.dart. Added correct icons and functionality.

--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gordos_pero_felizes/constants.dart';
 import 'package:gordos_pero_felizes/models/category.dart';
 import 'package:gordos_pero_felizes/widgets/category_card.dart';
+import 'package:gordos_pero_felizes/widgets/title_widget.dart';
 
 class UserScreen extends StatefulWidget {
   static final String screenId = 'userScreen';
@@ -16,62 +17,25 @@ class _UserScreenState extends State<UserScreen> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: k_whiteColor,
-        bottomSheet: BottomSheet(
-            onClosing: () {},
-            builder: (context) {
-              return SizedBox(
-                height: 20,
-              );
-            }
-            // TODO to be determined,
-            ),
         body: Container(
           padding: k_appPadding,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Icon(
-                    Icons.list,
-                    color: k_redColor,
-                    size: k_iconSize,
-                  ),
-                  Icon(
-                    Icons.account_circle,
-                    color: k_redColor,
-                    size: k_iconSize,
-                  ),
-                ],
-              ),
-              // TODO change this to user image
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 25),
-                child: Container(
-                  height: 80,
-                  child: Image.asset('images/gordos_logo.png'),
+              TitleWidget(
+                leftIcon: Icons.arrow_back,
+                onPressedLeftIcon: () => Navigator.pop(context),
+                mainText: 'Nombre Usuario',
+                textStyle: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 24,
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 15),
-                child: Text(
-                  'Nombre Usuario',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w700,
-                    fontSize: 24,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 15),
-                child: Text(
-                  'Tus Favoritos',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black,
-                    fontSize: 18,
-                  ),
+                isSecondaryText: true,
+                secondaryText: 'Tus Favoritos',
+                secondaryTextStyle: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
+                  fontSize: 18,
                 ),
               ),
               Expanded(
