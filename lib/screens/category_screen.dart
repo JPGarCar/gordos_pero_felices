caaf3eac27104bf48a0dcd3d441e05959dbaa6d6
Juan Pablo Garcia <juanpablo.garcia@alumni.ubc.ca>
2020-08-15 12:08:39 -0500

    Added title_widget.dart to category_screen.dart and checked app padding.

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:gordos_pero_felizes/models/business.dart';
 import 'package:gordos_pero_felizes/widgets/business_card.dart';
+import 'package:gordos_pero_felizes/widgets/title_widget.dart';
 
 import '../constants.dart';
 
@@ -31,45 +32,16 @@ class _CategoryScreenState extends State<CategoryScreen> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: k_whiteColor,
-        bottomSheet: BottomSheet(
-            onClosing: () {},
-            builder: (context) {
-              return SizedBox(
-                height: 20,
-              );
-            }
-            // TODO to be determined,
-            ),
         body: Container(
           padding: k_appPadding,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Icon(
-                    Icons.list,
-                    color: k_redColor,
-                    size: k_iconSize,
-                  ),
-                  Icon(
-                    Icons.account_circle,
-                    color: k_redColor,
-                    size: k_iconSize,
-                  ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 25),
-                child: Container(
-                  height: 80,
-                  child: Image.asset('images/gordos_logo.png'),
-                ),
-              ),
-              Text(
-                'Restaurantes Americanos',
-                style: TextStyle(
+              TitleWidget(
+                leftIcon: Icons.list,
+                rightIcon: Icons.account_circle,
+                mainText: 'Restauratnes Americandos',
+                textStyle: TextStyle(
                   fontWeight: FontWeight.w500,
                   color: Colors.black,
                   fontSize: 20,
