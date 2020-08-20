494673654c9ca302870e470a934a71037829066a
Juan Pablo Garcia <juanpablo.garcia@alumni.ubc.ca>
2020-08-20 14:49:12 -0500

    Add a cool animation to red_rounded_button.dart when pressed.

--- lib/widgets/red_rounded_button.dart
+++ lib/widgets/red_rounded_button.dart
@@ -2,17 +2,45 @@ import 'package:flutter/material.dart';
 
 import '../constants.dart';
 
-class RedRoundedButton extends StatelessWidget {
+class RedRoundedButton extends StatefulWidget {
   final Function onTapFunction;
   final String buttonText;
 
   RedRoundedButton({@required this.onTapFunction, this.buttonText = ''});
 
   @override
+  _RedRoundedButtonState createState() => _RedRoundedButtonState();
+}
+
+class _RedRoundedButtonState extends State<RedRoundedButton> {
+  bool isSelected = false;
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onTapFunction,
-      child: Container(
+      onTapDown: (details) {
+        setState(() {
+          isSelected = true;
+        });
+      },
+      onTapUp: (details) {
+        setState(() {
+          isSelected = false;
+        });
+      },
+      onTap: () {
+        setState(() {
+          isSelected = true;
+        });
+        widget.onTapFunction();
+      },
+      child: AnimatedContainer(
+        onEnd: () {
+          setState(() {
+            isSelected = false;
+          });
+        },
+        duration: Duration(milliseconds: 350),
         height: 35,
         padding: EdgeInsets.only(left: 15, right: 15, top: 8, bottom: 8),
         decoration: BoxDecoration(
@@ -21,12 +49,12 @@ class RedRoundedButton extends StatelessWidget {
           boxShadow: [
             BoxShadow(
               color: k_redColorLight,
-              offset: Offset.fromDirection(.8, 8),
+              offset: Offset.fromDirection(.8, isSelected ? 0 : 8),
             ),
           ],
         ),
         child: Text(
-          buttonText,
+          widget.buttonText,
           style: TextStyle(
             color: k_whiteColor,
             fontSize: 16,
