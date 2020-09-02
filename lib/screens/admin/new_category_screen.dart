3f382820fec8a0347d751b6cd9d6496fe13396b6
Juan Pablo Garcia <juanpablo.garcia@alumni.ubc.ca>
2020-09-02 14:13:31 -0500

    new_category_screen.dart can now choose businesses to add to category.

--- lib/screens/admin/new_category_screen.dart
+++ lib/screens/admin/new_category_screen.dart
@@ -4,10 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:gordos_pero_felizes/constants.dart';
 import 'package:gordos_pero_felizes/firebase_constants.dart';
 import 'package:gordos_pero_felizes/models/category.dart';
+import 'package:gordos_pero_felizes/services/dropdown_items_getter.dart';
 import 'package:gordos_pero_felizes/services/image_getter.dart';
 import 'package:gordos_pero_felizes/widgets/card/category_card.dart';
 import 'package:gordos_pero_felizes/widgets/dialogs/confirm_dialog.dart';
 import 'package:gordos_pero_felizes/widgets/red_rounded/red_rounded_button.dart';
+import 'package:gordos_pero_felizes/widgets/red_rounded/red_rounded_dropdown.dart';
 import 'package:gordos_pero_felizes/widgets/red_rounded/red_rounded_switch.dart';
 import 'package:gordos_pero_felizes/widgets/red_rounded/red_rounded_text_field.dart';
 import 'package:gordos_pero_felizes/widgets/title_widget.dart';
@@ -28,8 +30,25 @@ class _NewCategoryScreenState extends State<NewCategoryScreen> {
   /// var to know if new category is for home screen!
   bool isSpecial = false;
 
+  /// List of businesses to add to category
+  List<String> businessIDs = List<String>();
+
+  /// Drop down items and query of all businesses
+  List<DropdownMenuItem> dropDownItems;
+  String businessID;
+  QuerySnapshot querySnapshot;
+
   /// Deals with uploading category info to firestore
   Future uploadCategory(String name) async {
+    // Get document references of businesses choosen
+    List<DocumentReference> list = List<DocumentReference>();
+
+    querySnapshot.docs
+        .where((element) => businessIDs.contains(element.id))
+        .forEach((element) {
+      list.add(element.reference);
+    });
+
     String assetPath = await ImageGetter.uploadImage(
         image: _image, imagePath: 'categories/$name');
     _firestore
@@ -39,15 +58,33 @@ class _NewCategoryScreenState extends State<NewCategoryScreen> {
         .set({
       fk_categoryImageAssetPath: assetPath,
       fk_categoryName: name,
-      fk_businesses: List<DocumentReference>(),
+      fk_businesses: list,
       fk_isActive: isActive,
     });
+  }
+
+  /// Will get all the possible businesses for the drop down
+  void getBusinesses() {
+    DropDownItemsGetter.getBusinesses(
+        firebaseFirestore: _firestore,
+        thenFinalFunction: (items, value) {
+          querySnapshot = value;
+          setState(() {
+            dropDownItems = items;
+          });
+        });
   }
 
   @override
   void dispose() {
     nameController.dispose();
     super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getBusinesses();
   }
 
   @override
@@ -110,6 +147,51 @@ class _NewCategoryScreenState extends State<NewCategoryScreen> {
                           ],
                         ),
                       ),
+                      dropDownItems != null
+                          ? Padding(
+                              padding: EdgeInsets.symmetric(vertical: 10),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  RedRoundedDropDown(
+                                    dropDownItems: dropDownItems,
+                                    value: businessID,
+                                    hint: 'Negocio a agregar',
+                                    onChangeFunction: (value) {
+                                      setState(() {
+                                        businessIDs.add(value);
+                                        dropDownItems.removeWhere((element) =>
+                                            element.value == value);
+                                      });
+                                    },
+                                  ),
+                                  Flexible(
+                                    child: ListView.builder(
+                                      shrinkWrap: true,
+                                      itemCount: businessIDs.length,
+                                      itemBuilder: (context, index) {
+                                        return BusinessListItem(
+                                          businessID: businessIDs[index],
+                                          onTapFunction: () {
+                                            setState(() {
+                                              dropDownItems.add(
+                                                DropdownMenuItem(
+                                                  value: businessIDs[index],
+                                                  child:
+                                                      Text(businessIDs[index]),
+                                                ),
+                                              );
+                                              businessIDs.removeAt(index);
+                                            });
+                                          },
+                                        );
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+                          : SizedBox(),
                       RedRoundedSwitch(
                         text: 'Activo?',
                         value: isActive,
@@ -156,6 +238,33 @@ class _NewCategoryScreenState extends State<NewCategoryScreen> {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class BusinessListItem extends StatelessWidget {
+  final String businessID;
+  final Function onTapFunction;
+
+  BusinessListItem({this.businessID, this.onTapFunction});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            businessID,
+            style: k_16wStyle,
+          ),
+          GestureDetector(
+            child: Icon(Icons.remove_circle),
+            onTap: onTapFunction,
+          ),
+        ],
       ),
     );
   }
